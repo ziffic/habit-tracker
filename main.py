b155bb230c3ce759c056eec76d51579d352cacbc
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-30 16:43:40 -0700

    added first pixel

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import connect
 
 # https://pixe.la/v1/users/ziffic/graphs/graph1.html
 USER_NAME = "ziffic"
+GRAPH_ID = "graph1"
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USER_NAME}/graphs"
+PIXEL_ENDPOINT = f"{GRAPH_ENDPOINT}/{GRAPH_ID}"
 
 user_params = {
     "token": connect.PIXELA_TOKEN,
@@ -18,13 +20,20 @@ user_params = {
 # print(response.text)
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Dr Pepper Graph",
     "unit": "bottles",
     "type": "float",
     "color": "sora"
 }
 
-response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=connect.headers)
-print(response.text)
+# response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=connect.headers)
+# print(response.text)
 
+pixel_config = {
+    "date": "20230330",
+    "quantity": "2.0"
+}
+
+response = requests.post(url=PIXEL_ENDPOINT, json=pixel_config, headers=connect.headers)
+print(response.text)
