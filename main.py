20209a1016fefad9b2285f5a2854c97f519fa943
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-30 16:31:30 -0700

    created graph

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 import connect
 
+# https://pixe.la/v1/users/ziffic/graphs/graph1.html
+USER_NAME = "ziffic"
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USER_NAME}/graphs"
 
 user_params = {
     "token": connect.PIXELA_TOKEN,
-    "username": "ziffic",
+    "username": USER_NAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
@@ -13,4 +16,15 @@ user_params = {
 # Create user
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
 # print(response.text)
+
+graph_config = {
+    "id": "graph1",
+    "name": "Dr Pepper Graph",
+    "unit": "bottles",
+    "type": "float",
+    "color": "sora"
+}
+
+response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=connect.headers)
+print(response.text)
 
